759727392083508ea65951e29d92760ed7958147
林嘉裕 <42762318+codinglin@users.noreply.github.com>
2021-01-29 01:07:05 +0800

    OSS

--- guli_parent/service/service_oss/src/main/java/com/study/oss/OSSApplication.java
+++ guli_parent/service/service_oss/src/main/java/com/study/oss/OSSApplication.java
@@ -2,8 +2,10 @@ package com.study.oss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.study")
 public class OSSApplication {
     public static void main(String[] args) {
         SpringApplication.run(OSSApplication.class);
