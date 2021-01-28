f477712b8eadf9046f8863034d027b0c279b21ec
林嘉裕 <42762318+codinglin@users.noreply.github.com>
2021-01-29 00:16:45 +0800

    OSS配置

---
+++ guli_parent/service/service_oss/src/main/java/com/study/oss/OSSApplication.java
@@ -0,0 +1,11 @@
+package com.study.oss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OSSApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OSSApplication.class);
+    }
+}
